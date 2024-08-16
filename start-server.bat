2ad9f29b71840fcaa8bd6a4397a567ba4f40862f
Tuan Le <tuanle200577@gmail.com>
2024-08-16 18:17:34 -0500

    reorg codebase

--- start-server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Users\Tuan Le\OneDrive\Desktop\code projects\leetcode-tracker"
-start "" /b node server.js
-
-:: Open the web application in the default browser
-@REM start http://localhost:3000/
